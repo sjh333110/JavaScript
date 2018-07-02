90f095a0afa42e3f34cda1da773d30e8fc95cd50
song_my <song_my@example.con>
2018-07-02 17:59:45 +0800

    JavaScript

---
+++ Queue/000_Queue_closure.js
@@ -0,0 +1,32 @@
+let Queue = (function () {
+	const items = new WeakMap();
+	class Queue {
+		constructor () {
+			items.set(this, []);
+		};
+		isEmpty() {
+			let q = items.get(this);
+			return q.length == 0;
+		};
+		enqueue(element) {
+			let q = items.get(this);
+			q.push(element);
+		};
+		dequeue() {
+			let q = items.get(this);
+			let r = q.shift();
+			return r;
+		};
+		front() {
+			let q = items.get(this);
+			let r = q[0];
+		};
+		print() {
+			let q = items.get(this);
+			let r = q.toString();
+			return r;
+		};
+		
+	}
+	return Queue;
+})();
