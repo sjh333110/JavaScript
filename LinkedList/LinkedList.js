cabca3e66a9c7a2723b44daaba8a92ada8f8a2f1
song_my <song_my@example.con>
2018-07-12 18:04:59 +0800

    添加注释

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -1,75 +1,76 @@
 class LinkedList {
-    constructor() {
+    constructor () {
         let Node = function (element) {
             this.element = element;
             this.next = null;
-        };
-        let length = 0;
+        } 
+        let lenght = 0;
         let head = null;
-        //链表队尾添加元素
+        
+        //向链表尾部插入元素
         this.append = function (element) {
-            let node = new Node(element), current;
-            if (head == null) {             //判断是否存在元素
+            let node = new Node(element),current;
+            if (head == null) {
                 head = node;
             } else {
                 current = head;
-                while (current.next) {
+                while (current.next){
                     current = current.next;
                 }
                 current.next = node;
             }
-            length++;
+            lenght++;
         };
-        //插入元素
+        // 往链表的任意位置插入元素
         this.insert = function (position, element) {
-            if (position >= 0 && position <= length) {
-                let node = new Node(element), current = head, previous, index = 0;
+            if(position >=0 && position <=lenght) {
+                let node = new Node(element), current = head, previous, index=0;
                 if (position === 0) {
-                    node.next = current;
-                    head = node;
+                   node.next = current;
+                   head = node;
                 } else {
                     while (index++ < position) {
                         previous = current;
                         current = current.next;
                     }
-                    node.next = current;
                     previous.next = node;
+                    node.next = current;
                 }
-                length++;
+                lenght++;
                 return true;
-            }
-            else {
-                return false;
+            } else {
+                return fasle;
             }
         };
+        // 按照位置移除元素
         this.removeAt = function (position) {
-            if (position > -1 && position < length) {
+            if(position >-1 && position <= lenght){
                 let current = head, previous, index = 0;
                 if (position === 0) {
                     head = current.next;
-                }
-                else {
+                } else {
                     while (index++ < position) {
                         previous = current;
                         current = current.next;
                     }
                     previous.next = current.next;
                 }
-                lenght--;
+                lenght--; 
                 return current.element;
-            }
-            else {
+            } else {
                 return null;
             }
         };
+        // 按照元素值删除元素
         this.remove = function (element) {
             let index = this.indexOf(element);
-            return this.removeAt(index);
+            this.removeAt(index);
         };
+        // 判断链表是否包含某元素
         this.indexOf = function (element) {
             let current = head, index = 0;
             while (current) {
-                if (element === current.element) {
+                if(element === current.element){
                     return index;
                 }
                 index++;
@@ -77,25 +78,31 @@ class LinkedList {
             }
             return -1;
         };
+        // 判断链表是否为空
         this.isEmpty = function () {
-            return length === 0;
+            return lenght === 0;
         };
+        // 获取链表长度
         this.size = function () {
-            return length;
+            return lenght;
         };
+        // 获取链表首个元素
         this.getHead = function () {
             return head;
         };
+        // 将链表转换为字符串
         this.toString = function () {
-            let current = head, string = '';
+            let current = head,string = '';
             while (current) {
                 string += current.element + (current.next ? ',' : '');
                 current = current.next;
             }
-            return string;
+            return string
         };
-        this.print = function () { 
-            this.toString();
+        // 打印链表元素
+        this.print = function () {
+            console.log(this.toString());
         };
+
     }
 }
